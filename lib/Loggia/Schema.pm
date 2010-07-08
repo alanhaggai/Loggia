38a9be3ea503718b63a826839c6f4c326567b294
Alan Haggai Alavi <alanhaggai@alanhaggai.org>
2010-07-08 07:22:19 +0530

    created model
    Schema for album table has also been included.

---
+++ lib/Loggia/Schema.pm
@@ -0,0 +1,16 @@
+package Loggia::Schema;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2010-07-08 07:22:12
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:FLeV14SHkdIBvQc+zNgk1w
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
---
+++ lib/Loggia/Schema/Result/Album.pm
@@ -0,0 +1,37 @@
+package Loggia::Schema::Result::Album;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "Core");
+__PACKAGE__->table("album");
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "INT", default_value => undef, is_nullable => 0, size => 11 },
+  "name",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 100,
+  },
+  "description",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 255,
+  },
+);
+__PACKAGE__->set_primary_key("id");
+__PACKAGE__->add_unique_constraint("name", ["name"]);
+
+
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2010-07-08 07:22:12
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:cFbTpdYsbqR8X1lbUSJaWA
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
