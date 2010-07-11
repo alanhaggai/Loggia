8953c755623291191ee7edcc2a5ed0b2323ef8b4
Alan Haggai Alavi <alanhaggai@alanhaggai.org>
2010-07-11 23:31:12 +0530

    added class for schema 'image'

---
+++ lib/Loggia/Schema/Result/Image.pm
@@ -0,0 +1,38 @@
+package Loggia::Schema::Result::Image;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "Core");
+__PACKAGE__->table("image");
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "INT", default_value => undef, is_nullable => 0, size => 11 },
+  "description",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 255,
+  },
+  "hash",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 32,
+  },
+  "album",
+  { data_type => "INT", default_value => undef, is_nullable => 1, size => 11 },
+);
+__PACKAGE__->set_primary_key("id");
+
+
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2010-07-11 23:28:05
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:IvYx6WUQeaKnyNvgc7V8Dw
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
